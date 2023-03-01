f828ca028d1d5f08138e2a5a84094fa12b61ea2c
sunnyshin8 <shingloo55@gmail.com>
2023-03-01 11:55:43 +0530

    initial commit

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ for i in range(1, numberOfDays+1):
     print("change made to file.txt", i)
     os.system("git add .")
     print("git add .", i)
-    os.system(f"git commit --date=\"2023-02-01 11:55:43\" -m \"initial commit\"")
+    os.system(f"git commit --date=\"2023-02-29 11:55:43\" -m \"initial commit\"")
     print("git commit", i)
 
 #git push
