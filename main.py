dfda9a13005df17ac65892ff07f575642e97589d
sunnyshin8 <shingloo55@gmail.com>
2024-08-21 11:55:43 +0530

    initial commit

--- main.py
+++ main.py
@@ -2,22 +2,25 @@ from changes import write
 import os
 import datetime
 
-numberOfDays = 3
+numberOfDays= 3
 
-# Set the desired date
-target_date = datetime.datetime(2024, 8, 21, 11, 55, 43)
+# #change
+# for i in range(numberOfDays):
+#     write(f"Day {i+1} of {numberOfDays} days of code\n")
 
-for i in range(1, numberOfDays + 1):
+# #git add .
+# os.system("git add .")
+
+#git commit
+for i in range(1, numberOfDays+1):
     write(f"Day {i+1} of {numberOfDays} days of code\n")
     print("change made to file.txt", i)
     os.system("git add .")
     print("git add .", i)
-
-    # Use the target date for the commit
-    commit_message = f"Initial commit (Day {i+1})"
-    os.system(f'git commit --date="{target_date.strftime("%2024-%08-%22 %7:%22:%42")}" -m "{commit_message}"')
+    
+    os.system(f"git commit --date=\"2024-08-21  b 11:55:43\" -m \"initial commit\"")
     print("git commit", i)
 
-# Push to the remote repository
+#git push
 os.system("git push")
 print("git push")
